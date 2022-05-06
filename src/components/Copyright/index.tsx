48cb209f31388dd1a54f9189a06bdc8d4a5827c6
Wanderson Santos <wanderson.santos@gcbinvestimentos.com>
2022-05-06 13:05:48 -0300

    feat: options components

--- src/components/Copyright/index.tsx
+++ src/components/Copyright/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 import { styles } from './styles';
 
 export function Copyright() {
   return (
-    <View style={styles.container}>
-
+    <View>
+        <Text style={styles.text}>
+            Feito por Wanderson Santos
+        </Text>
     </View>
   );
 }
--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -4,6 +4,9 @@ import { ChatTeardropDots } from "phosphor-react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
+import { Options } from "../Options";
+
+
 import { styles } from "./styles";
 import { theme } from "../../theme";
 
@@ -24,12 +27,13 @@ function Widget() {
         />
       </TouchableOpacity>
 
-      <BottomSheet ref={bottomSheetRef}
-      snapPoints={[1, 280]}
-      backgroundStyle={styles.modal}
-      handleIndicatorStyle={styles.indicator}
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
       >
-        
+        <Options />
       </BottomSheet>
     </>
   );
